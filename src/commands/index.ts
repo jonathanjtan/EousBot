ccf368ce33744f2b4b7919db77bfeae041f166fd
EousBot <eousbot@users.noreply.github.com>
2026-08-04 22:49:49 +0000

    create an /8ball command
    Implements #3.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,4 +1,5 @@
 import { command as build } from "./build.js";
+import { command as eightball } from "./eightball.js";
 import { command as ping } from "./ping.js";
 import { command as request } from "./request.js";
 import { command as roll } from "./roll.js";
@@ -12,7 +13,7 @@ import type { Command } from "./types.js";
  * has to edit this file, which makes every new command visible in the PR diff
  * instead of appearing by filesystem side effect.
  */
-export const commands: Command[] = [ping, request, status, build, roll];
+export const commands: Command[] = [ping, request, status, build, roll, eightball];
 
 export const commandsByName = new Map(commands.map((c) => [c.data.name, c]));
 
